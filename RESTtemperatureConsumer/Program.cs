16086c176954cb3091397b3be083a21b062d0f3f
Anders Børjesson <anbo@zealand.dk>
2021-05-13 09:13:05 +0200

    Add project files.

---
+++ RESTtemperatureConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RESTtemperatureConsumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            string uri = "https://simonappservice.azurewebsites.net/api/Temp";
+            for (int i = 0; i < 10; i++)
+            {
+                Temp t = new Temp { Temperatur = "5" + i };
+                Temp addedTemp = RESTClient.Post<Temp, Temp>(uri, t).Result;
+                Console.WriteLine("temp: " + addedTemp);
+                // Pt har jeres REST POST en tom response body.
+                // bedre hvis den har Temp objekt i body
+            }
+        }
+    }
+}
---
+++ RESTtemperatureConsumer/Temp.cs
@@ -0,0 +1,13 @@
+﻿namespace RESTtemperatureConsumer
+{
+    class Temp
+    {
+        public int Id { get; set; }
+        public string Temperatur { get; set; }
+
+        public override string ToString()
+        {
+            return Id + " " + Temperatur;
+        }
+    }
+}
